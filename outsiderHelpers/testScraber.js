0d139e4a6b03e91531e9fdb789470196a63f2155
mahmoud-bored <mahmoud.public.404@gmail.com>
2023-10-18 08:31:07 +0300

    Updated: testBanks.json according to Firebase starte for now, still need to update bank 2 and 3 in firebase and  testBanks Added: new scrabber in testScrabber.js for scrabbing html element not just text content which solves the words sticking problems Styled: more space between Paragraph sections

--- outsiderHelpers/testScraber.js
+++ outsiderHelpers/testScraber.js
@@ -189,3 +189,69 @@ document.querySelectorAll('.Qr7Oae > .OxAavc').forEach(elmntsContainer => {
     }
 })
 console.log(JSON.stringify(mainObj, null, 2))
+
+
+
+
+
+
+
+// Answers With Paragraph Scrabber V2
+console.log('=========================================================================================================================')
+i = -1
+mainObj = {
+    test1:{
+      info:{
+        name: document.querySelector('.LgNcQe, .LgNcQe .Wic03c .tL9Q4c, .LgNcQe .I9OJHe .KRoqRc, .LgNcQe .PyrB4, .LgNcQe .snByac').textContent
+      },
+      questions: {
+        
+      }
+    }
+}
+document.querySelectorAll('.Qr7Oae > .OxAavc').forEach(elmntsContainer => {
+    if(!elmntsContainer.previousElementSibling?.querySelector('.SajZGc.w0W4bd.tT7zXd.RVEQke') && !elmntsContainer.querySelector('.Eg06Ee > .hfh9V > .ujnDW > .docssharedWizToggleLabeledContainer.rnFeqc.LygNqb.N2RpBe.RDPZE > .bzfPab.wFGF8 > .uVccjd.aiSeRd.FXLARc.wGQFbe.BJHAP.NCCCpb.oLlshd.N2RpBe.RDPZE')){
+        // Get Question Paragraph
+        if(elmntsContainer.classList.contains('NVbRL')){
+            i++
+            k = 0
+            mainObj.test1['questions'][`${i}`] = {}
+            mainObj.test1['questions'][`${i}`]['questionType'] = "long"
+            let fullParagraphStr
+            if(elmntsContainer.querySelector('.pYfr3c > .prOLdf > .g53bl > .cTDvob.D1wxyf.RjsPE > .M7eMe ')){
+                fullParagraphStr = elmntsContainer.querySelector('.pYfr3c > .prOLdf > .g53bl > .cTDvob.D1wxyf.RjsPE > .M7eMe').innerHTML
+            }
+
+            mainObj.test1['questions'][`${i}`]['questionParagraph'] = fullParagraphStr
+            mainObj.test1['questions'][`${i}`]['questions'] = {}
+        }else{
+            mainObj.test1['questions'][`${i}`]['questions'][`${k}`] = {}
+            // Get Question Header
+          	let fullQuestionStr = ''
+            let isQuestion = elmntsContainer.querySelector('.pYfr3c > .prOLdf > .g53bl > .cTDvob.D1wxyf.RjsPE > span:nth-child(1)')
+            if(isQuestion){	
+								fullQuestionStr = elmntsContainer.querySelector('.pYfr3c > .prOLdf > .g53bl > .cTDvob.D1wxyf.RjsPE > span').innerHTML
+              	mainObj.test1['questions'][`${i}`]['questions'][`${k}`]['question'] = fullQuestionStr
+
+            }
+
+            // Get Question Answers
+            let j = 0
+            mainObj.test1['questions'][`${i}`]['questions'][`${k}`]['answers'] = {}
+            elmntsContainer.querySelectorAll('div > .lLfZXe.fnxRtf.cNDBpf > .H2Gmcc.tyNBNd > .gEPCre.D5c69b > .yUJIWb').forEach(elmntAnswer=>{
+                mainObj.test1['questions'][`${i}`]['questions'][`${k}`]['answers'][`${j}`] = elmntAnswer.querySelector('.docssharedWizToggleLabeledContainer > .bzfPab.wFGF8 > .YEVVod > .ulDsOb > .aDTYNe.snByac.kTYmRb.OIC90c').innerHTML
+                j++
+            })
+            // Get Question Right Answer
+            if(elmntsContainer.querySelector('.pYfr3c').classList.contains('LRmqmf')){
+                mainObj.test1['questions'][`${i}`]['questions'][`${k}`]['rightAnswer'] = elmntsContainer.querySelector('div > .lLfZXe.fnxRtf.cNDBpf > .H2Gmcc.tyNBNd > .gEPCre.D5c69b > .yUJIWb > .docssharedWizToggleLabeledContainer.O4MBef.LygNqb.N2RpBe.A1MUVb.tXA2Db.RDPZE > .bzfPab.wFGF8 > .YEVVod > .ulDsOb > .aDTYNe.snByac.kTYmRb.OIC90c').innerHTML
+            
+            }else if(elmntsContainer.querySelector('.D42QGf > .muwQbd > .fiH1oe > .docssharedWizToggleLabeledContainer > .bzfPab.wFGF8 > .YEVVod > .ulDsOb > .aDTYNe.snByac.kTYmRb.OIC90c')){
+                mainObj.test1['questions'][`${i}`]['questions'][`${k}`]['rightAnswer'] = elmntsContainer.querySelector('.D42QGf > .muwQbd > .fiH1oe > .docssharedWizToggleLabeledContainer > .bzfPab.wFGF8 > .YEVVod > .ulDsOb > .aDTYNe.snByac.kTYmRb.OIC90c').innerHTML
+            }
+            k++
+        }
+
+    }
+})
+console.log(JSON.stringify(mainObj, null, 2) + ',')
